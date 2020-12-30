31cd1f5c6072216e457e2f0d312e34f9f6c7d483
TarasOstasha <tonyjoss1990@gmail.com>
2020-12-29 21:41:37 -0500

    add cart

---
+++ routes/carts.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+// receive a POST request to add item to a cart
+
+// receive a GET request to show all items in cart
+
+// receive a POST request to delete an item from a cart
+
+module.exports = router;
